81f9462376961fe4895829788ae1e9d259a2c0f6
Jackson Morgan <jake81morgan@gmail.com>
2024-02-29 13:12:36 -0600

    Update AbstractPizza.java
    added constructor, setters and getters, as well as the protected attributes

--- src/edu/mu/PizzaProject/Pizzas/AbstractPizza.java
+++ src/edu/mu/PizzaProject/Pizzas/AbstractPizza.java
@@ -1,5 +1,87 @@
 package edu.mu.PizzaProject.Pizzas;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.mu.PizzaProject.Enums.*;
+import edu.mu.PizzaProject.Strategies.ICookingStrategy;
+
 public abstract class AbstractPizza {
 
+	protected List<Toppings> toppingList;
+	protected double priceWithoutToppings;
+	protected double totalPrice;
+	protected int pizzaOrderID;
+	protected static int orderIDCounter;
+	protected ICookingStrategy cookingStrategy;
+	protected double cookingPrice;
+	
+	/**
+	 * TODO: Constructor for AbstractPizza class
+	 */
+	public AbstractPizza() {
+		
+		toppingList = new ArrayList<Toppings>();
+		
+	}
+
+	// Getters and Setters
+	public List<Toppings> getToppingList() {
+		return toppingList;
+	}
+
+	public void setToppingList(List<Toppings> toppingList) {
+		this.toppingList = toppingList;
+	}
+
+	public double getPriceWithoutToppings() {
+		return priceWithoutToppings;
+	}
+
+	public void setPriceWithoutToppings(double priceWithoutToppings) {
+		this.priceWithoutToppings = priceWithoutToppings;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public int getPizzaOrderID() {
+		return pizzaOrderID;
+	}
+
+	public void setPizzaOrderID(int pizzaOrderID) {
+		this.pizzaOrderID = pizzaOrderID;
+	}
+
+	public static int getOrderIDCounter() {
+		return orderIDCounter;
+	}
+
+	public static void setOrderIDCounter(int orderIDCounter) {
+		AbstractPizza.orderIDCounter = orderIDCounter;
+	}
+
+	public ICookingStrategy getCookingStrategy() {
+		return cookingStrategy;
+	}
+
+	public void setCookingStrategy(ICookingStrategy cookingStrategy) {
+		this.cookingStrategy = cookingStrategy;
+	}
+
+	public double getCookingPrice() {
+		return cookingPrice;
+	}
+
+	public void setCookingPrice(double cookingPrice) {
+		this.cookingPrice = cookingPrice;
+	}
+	
+	
+	
 }
