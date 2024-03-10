501a1de66176f52b78c6ac32e2427ac725458d83
joeyg <joeyg@Joey_Desktop>
2024-03-10 18:11:42 -0500

    Updated Main to test each pizza type

--- src/edu/mu/PizzaProject/Main.java
+++ src/edu/mu/PizzaProject/Main.java
@@ -6,11 +6,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Testing the classes
+        MargheritaPizza margheritaPizza = new MargheritaPizza(0.00);
+        System.out.println("Margherita Pizza:");
+        System.out.println(margheritaPizza);
 
-		AbstractPizza MargheritaPizza = new MargheritaPizza(0.00);
-		String PrintInformation = MargheritaPizza.toString();
-		System.out.println(PrintInformation);
-		
+        VegetarianPizza vegetarianPizza = new VegetarianPizza(0.00);
+        System.out.println("\nVegetarian Pizza:");
+        System.out.println(vegetarianPizza);
+
+        HawaiianPizza hawaiianPizza = new HawaiianPizza(0.00);
+        System.out.println("\nHawaiian Pizza:");
+        System.out.println(hawaiianPizza);
+
+        SupremePizza supremePizza = new SupremePizza(0.00);
+        System.out.println("\nSupreme Pizza:");
+        System.out.println(supremePizza);
 
 	}
 
