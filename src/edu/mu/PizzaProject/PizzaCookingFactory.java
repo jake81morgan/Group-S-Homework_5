d186502e9ae2ff61b115c972dba9db99cb685322
Pablo Lasarte <pl8n4@umsystem.edu>
2024-03-11 22:33:36 -0500

    Updated PizzaCookingFactory class

--- src/edu/mu/PizzaProject/PizzaCookingFactory.java
+++ src/edu/mu/PizzaProject/PizzaCookingFactory.java
@@ -1,5 +1,24 @@
 package edu.mu.PizzaProject;
 
+import edu.mu.PizzaProject.Enums.PizzaType;
+import edu.mu.PizzaProject.Pizzas.*;
+
 public class PizzaCookingFactory {
 
+	
+	public AbstractPizza createPizza(PizzaType pizzaType) {
+		switch (pizzaType) {
+			case HAWAIIAN:
+				return  new HawaiianPizza();
+			case MARGHERITA:
+				return  new MargheritaPizza();
+			case SUPREME:
+				return new SupremePizza();
+			case VEGETARIAN:
+				return new VegetarianPizza();
+			default:
+				throw new IllegalArgumentException("Invalid Pizza Type" + pizzaType);
+		}
+
+	}
 }
